726e04fcc92200b95980e851a932e569235c6160
Erkan <ozturk.erkan96@gmail.com>
2017-11-28 18:15:40 +0300

    Proje dosyası ekle.

---
+++ Aygıt Yöneticisi/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+' denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+' bu öznitelik değerlerini değiştirin.
+
+' Bütünleştirilmiş kod özniteliklerinin değerlerini gözden geçirin
+
+<Assembly: AssemblyTitle("Aygıt Yöneticisi")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Aygıt Yöneticisi")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+<Assembly: Guid("c69279d5-0655-4f30-baa6-80caeefe6605")>
+
+' Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+'
+'      Ana Sürüm
+'      İkincil Sürüm 
+'      Yapı Numarası
+'      Düzeltme
+'
+' Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+' '*' kullanarak varsayılana ayarlayabilirsiniz:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
